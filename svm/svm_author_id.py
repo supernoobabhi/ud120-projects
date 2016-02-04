46c07b49010a97f0e65166726f36d53e091f2229
supernoobabhi <abhijeet_chauhan@yahoo.co.in>
2016-02-04 20:04:03 +0530

    Implemented SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -21,9 +21,30 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
 
 #########################################################
 ### your code goes here ###
+
+# Using svm
+from sklearn.svm import SVC
+clf = SVC(C=10000,kernel='rbf')
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+pre=clf.predict(features_test);
+print "predicting time:", round(time()-t0, 3), "s"
+# Checking accuracy
+from sklearn.metrics import accuracy_score
+print len(pre)
+ans=0
+for i in range (0,len(pre)):
+  if(pre[i]==1):
+    ans=ans+1
+print ans
+print(accuracy_score(labels_test, pre))
 
 #########################################################
 
